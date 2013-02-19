1bb19f66e49e9c8af8d689732c8d15470cd34f0c
gongming <mg90@foxmail.com>
2013-02-19 13:12:41 +0800

    rm file not need

--- album/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from album.views import *
-
-urlpatterns = patterns('',
-    ('^$', album),
-    ('^(P<albumm_id>.{0,})/$', album),
-    ('^test/$', album),
-)
